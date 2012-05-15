128776504d3d95a7f117d839ac3a5ba79d160b2f
davincistudio <sunhokim@incross.com>
2012-05-15 23:02:03 +0900

    step1 step2

---
+++ Sample/step2/main.js
@@ -0,0 +1,14 @@
+﻿
+
+var count=0;
+	
+function Layoutview_0_onclick(e, id) {
+	console.log("Layoutview_0_onclick");
+	
+	var change = $.davinci.getInstance("#Label_0");
+	if(id=='Button_0'){
+		var txt = (count%2==0) ? "Hello World!!" : "Welcome to DaVinci!!";
+		change.text(txt);
+		count++;
+	}	
+}
